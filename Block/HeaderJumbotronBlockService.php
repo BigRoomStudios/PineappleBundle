dc6a6cd38b821409591c6c66844fb3534e096547
ThrivingKings <dan@bigroomstudios.com>
2014-09-04 16:15:34 -0400

    added wysiwyg to all current blocks

--- Block/HeaderJumbotronBlockService.php
+++ Block/HeaderJumbotronBlockService.php
@@ -4,18 +4,44 @@ namespace BRS\PineappleBundle\Block;
 
 use Sonata\PageBundle\Block\ChildrenPagesBlockService as base;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Sonata\BlockBundle\Block\BlockContextInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+
+use Sonata\BlockBundle\Model\BlockInterface;
+use Symfony\Component\Templating\EngineInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * 
  */
 class HeaderJumbotronBlockService extends base {
   
+  public function buildEditForm(FormMapper $formMapper, BlockInterface $block) {
+    
+    $formMapper->add('settings', 'sonata_type_immutable_array', array(
+      'label' => '',
+      'keys' => array(
+        array('content', 'textarea', array(
+          'attr' => array(
+            'class' => 'tinymce',
+            'id' => 'tinymce_'.$block->getId(),
+          ),
+        )),
+      )
+    ));
+    
+  }
+
   public function setDefaultSettings(OptionsResolverInterface $resolver) {
     
     parent::setDefaultSettings($resolver);
     
     $resolver->setDefaults(array(
-      'template' => 'BRSPineappleBundle:Blocks:header.jumbotron.html.twig'
+      'template' => 'BRSPineappleBundle:Blocks:header.jumbotron.html.twig',
+      'content' => '<h1>Jumbotron heading</h1><p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>',
     ));
   }
   
--- Block/HeaderSplashBlockService.php
+++ Block/HeaderSplashBlockService.php
@@ -4,6 +4,15 @@ namespace BRS\PineappleBundle\Block;
 
 use Sonata\PageBundle\Block\ChildrenPagesBlockService as base;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Sonata\BlockBundle\Block\BlockContextInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+
+use Sonata\BlockBundle\Model\BlockInterface;
+use Symfony\Component\Templating\EngineInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * 
@@ -12,10 +21,27 @@ class HeaderSplashBlockService extends base {
   
   public function setDefaultSettings(OptionsResolverInterface $resolver) {
     
+    public function buildEditForm(FormMapper $formMapper, BlockInterface $block) {
+    
+    $formMapper->add('settings', 'sonata_type_immutable_array', array(
+      'label' => '',
+      'keys' => array(
+        array('content', 'textarea', array(
+          'attr' => array(
+            'class' => 'tinymce',
+            'id' => 'tinymce_'.$block->getId(),
+          ),
+        )),
+      )
+    ));
+    
+  }
+
     parent::setDefaultSettings($resolver);
     
     $resolver->setDefaults(array(
-      'template' => 'BRSPineappleBundle:Blocks:header.splash.html.twig'
+      'template' => 'BRSPineappleBundle:Blocks:header.splash.html.twig',
+      'content' => '<h1>Welcome to the index</h1><p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>',
     ));
   }
   
