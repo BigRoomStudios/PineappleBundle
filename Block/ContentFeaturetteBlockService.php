e960811aff90cf61025ef8839cb5577630768d91
ThrivingKings <dan@bigroomstudios.com>
2014-09-04 16:45:41 -0400

    added flipping option

--- Block/ContentFeaturetteBlockService.php
+++ Block/ContentFeaturetteBlockService.php
@@ -24,6 +24,11 @@ class ContentFeaturetteBlockService extends base {
     $formMapper->add('settings', 'sonata_type_immutable_array', array(
       'label' => '',
       'keys' => array(
+        array('flip', 'choice', array(
+          'choices'   => array('n' => 'No', 'y' => 'Yes'),
+          'required'  => true,
+          'label' => 'Flip layout?'
+        )),
         array('content', 'textarea', array(
           'attr' => array(
             'class' => 'tinymce',
@@ -41,6 +46,7 @@ class ContentFeaturetteBlockService extends base {
     
     $resolver->setDefaults(array(
       'template' => 'BRSPineappleBundle:Blocks:content.featurette.html.twig',
+      'flip' => 'n',
       'content' => '<h2 class="featurette-heading">First featurette heading. <span class="text-muted">It will blow your mind.</span></h2><p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>',
     ));
   }
